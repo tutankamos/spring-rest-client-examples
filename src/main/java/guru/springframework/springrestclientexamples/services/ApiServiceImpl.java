44d13405cecc132651808903158bdde11b5a7b8b
Amos Torretta <torrettaamos@gmail.com>
2023-03-07 12:06:36 +0100

    getting data for my api from another api and test it

---
+++ src/main/java/guru/springframework/springrestclientexamples/services/ApiServiceImpl.java
@@ -0,0 +1,25 @@
+package guru.springframework.springrestclientexamples.services;
+
+import guru.springframework.api.domain.User;
+import guru.springframework.api.domain.UserData;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class ApiServiceImpl implements ApiService {
+
+    private RestTemplate restTemplate;
+
+    public ApiServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public List<User> getUser(Integer limit) {
+        List<User> userData = restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit=" + limit, List.class);
+
+        return userData;
+    }
+}
---
+++ src/test/java/guru/springframework/springrestclientexamples/services/ApiServiceImplTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.springrestclientexamples.services;
+
+import guru.springframework.api.domain.User;
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiServiceImplTest extends TestCase {
+
+    @Autowired
+    ApiService apiService;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
+        List<User> users = apiService.getUser(3);
+        assertEquals(3, users.size());
+
+    }
+}
